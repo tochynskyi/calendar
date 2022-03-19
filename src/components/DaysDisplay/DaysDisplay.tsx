3400571e2db1f0299ef19b239f72af82e68ed2a8
Serhii Tochynskyi <tochynskyi.s@gmail.com>
2022-03-19 15:10:58 +0200

    fix layout display days

--- src/components/DaysDisplay/DaysDisplay.tsx
+++ src/components/DaysDisplay/DaysDisplay.tsx
@@ -13,42 +13,75 @@ const DaysDisplay: FC<DaysDisplayProps> = ({ display }) => {
   const { currentMonth } = useAppSelector((state) => state.calendarSlice);
   const calendar = useCalendar(currentMonth);
   const date = getDate(currentMonth);
+  console.log(display);
 
   return (
-    <div className={style.calendar__body}>
+    <>
       {display === "small" && (
-        <div className={style.calendar__dayNames}>
-          {calendar[0].map(({ dayName }, i) => (
-            <div key={i} className={style.calendar__dayName}>
-              {dayName}
+        <div className={style.smallCalendar__wrapper}>
+          <div className={style.smallCalendar__dayNames}>
+            {calendar[0].map(({ dayName }, i) => (
+              <div key={i} className={style.smallCalendar__dayName}>
+                {dayName}
+              </div>
+            ))}
+          </div>
+          {calendar.map((week, i) => (
+            <div key={i} className={style.calendar__row}>
+              {week.map(({ year, month, day }, id) => (
+                <div
+                  key={id}
+                  className={`${style.calendar__day}
+        			${date.month === month && `${style.calendar__day__current}`}
+        			${isActiveDay(year, month, day) && `${style.calendar__day__active}`}
+        		`}
+                >
+                  {day}
+                </div>
+              ))}
             </div>
           ))}
         </div>
       )}
-      <div className={style.calendar__monthDaysDisplay}>
-        {calendar.map((week, i) => (
-          <div key={i} className={style.calendar__row}>
-            {week.map(({ year, month, day, dayName }, id) => (
-              <div className={`${display === "large" && `${style.calendar__day__large}`}`}>
-                {i === 0 && display === "large" && (
-                  <div key={dayName}>{dayName}</div>
-                )}
-                <div
-                  key={id}
-                  className={`${style.calendar__day}
-                     ${date.month === month && `${style.calendar__day__current}`}
-							${isActiveDay(year, month, day) && `${style.calendar__day__active}`}
-						`}
-                >
-                  {day}
-                </div>
-              </div>
-            ))}
-          </div>
-        ))}
-      </div>
-    </div>
+    </>
   );
 };
+
+//  <div className={style.calendar__body}>
+//    {display === "small" && (
+//      <div className={style.calendar__dayNames}>
+//        {calendar[0].map(({ dayName }, i) => (
+//          <div key={i} className={style.calendar__dayName}>
+//            {dayName}
+//          </div>
+//        ))}
+//      </div>
+//    )}
+//  <div className={style.calendar__monthDaysDisplay}>
+//    {calendar.map((week, i) => (
+//      <div key={i} className={style.calendar__row}>
+//        {week.map(({ year, month, day, dayName }, id) => (
+//          <div className={`
+// 		  ${display === "large" && `${style.calendar__day__large}`}
+// 		  ${display === "small" && `${style.calendar__day__small}`}
+// 		  `}>
+//            {i === 0 && display === "large" && (
+//              <div key={dayName}>{dayName}</div>
+//            )}
+//            <div
+//              key={id}
+//              className={`${style.calendar__day}
+//                 ${date.month === month && `${style.calendar__day__current}`}
+// 					${isActiveDay(year, month, day) && `${style.calendar__day__active}`}
+// 				`}
+//            >
+//              {day}
+//            </div>
+//          </div>
+//        ))}
+//      </div>
+//    ))}
+//  </div>
+//  </div>
 
 export default DaysDisplay;

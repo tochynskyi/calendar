0f8548f494c56223ee6e168121d2c6072ed049cc
Serhii Tochynskyi <tochynskyi.s@gmail.com>
2022-03-20 13:45:23 +0200

    do layout for large  calendar

--- src/components/LargeCalendar/LargeCalendar.tsx
+++ src/components/LargeCalendar/LargeCalendar.tsx
@@ -1,19 +1,21 @@
-import React, { FC } from 'react'
-import { useAppSelector } from '../../hooks/redux'
-import { useCalendar } from '../../hooks/useCalendar'
-import { getDate } from '../../utils/getDate'
-import DaysDisplay from '../DaysDisplay/DaysDisplay'
-import style from './LargeCalendar.module.scss'
+import React, { FC } from "react";
+import { useAppSelector } from "../../hooks/redux";
+import { useCalendar } from "../../hooks/useCalendar";
+import { getDate } from "../../utils/getDate";
+import DaysDisplay from "../DaysDisplay/DaysDisplay";
+import style from "./LargeCalendar.module.scss";
 
 const LargeCalendar: FC = () => {
-	const { currentMonth } = useAppSelector(state => state.calendarSlice)
-	const largeCalendar = useCalendar(currentMonth)
-	const date = getDate(currentMonth);
+  const { currentMonth } = useAppSelector((state) => state.calendarSlice);
+  const largeCalendar = useCalendar(currentMonth);
+  const date = getDate(currentMonth);
 
-	// Додати обгортку (дивитись в Гугл календар)
+  // Додати обгортку (дивитись в Гугл календар)
   return (
-	<DaysDisplay display='large' calendar={largeCalendar} date={date}/>
-  )
-}
+    <div className={style.calendar__wrapper}>
+      <DaysDisplay display="large" calendar={largeCalendar} date={date} />
+    </div>
+  );
+};
 
-export default LargeCalendar
+export default LargeCalendar;
